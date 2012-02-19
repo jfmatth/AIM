d883c74ba9a93ffffc61a3a233ed34fa9a31c53d
John Matthew <john@compunique.com>
2012-02-19 12:18:41 -0800

    changes for inheritance

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
                        
-    (r'^$', TemplateView.as_view(template_name="bootstrap.html")),
+    (r'^$', TemplateView.as_view(template_name="index.html")),
     
     # Example:
 #    (r'^aim/', include('aim.urls') ),
