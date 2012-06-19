6101d2ea02f87f27e1ca71c58040fed878832787
John Matthew <john@compunique.com>
2012-06-18 18:04:58 -0700

    Moving to django 1.3 very slowly

--- urls.py
+++ urls.py
@@ -1,27 +1,24 @@
-from django.conf.urls.defaults import *
-
+from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import TemplateView
-    
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
+class AboutView(TemplateView):
+    template_name = "bootstrap-sample.html"
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-                       
-    (r'^$', TemplateView.as_view(template_name="index.html")),
-    
-    # Example:
-#    (r'^aim/', include('aim.urls') ),
+    # Examples:
+    # url(r'^$', 'aim_v13.views.home', name='home'),
+    # url(r'^aim_v13/', include('aim_v13.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^bootstrap/', AboutView.as_view()),
+    
 )
-
